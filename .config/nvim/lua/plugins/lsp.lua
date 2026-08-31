67a5f8364b66a2694a68e40cdbd2ff31e89fb2b4
saka404 <67270824+Saka-CS@users.noreply.github.com>
2026-08-31 16:40:43 +0300

    Configured both Neovim and Kanata

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -15,3 +15,78 @@
 --     },
 --   },
 -- }
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ruff = {
+        -- cmd_env = { RUFF_TRACE = "messages" },
+        init_options = {
+          settings = {
+            -- General & Logging
+            -- logLevel = "info",
+            -- showSyntaxErrors = true,
+            -- configurationPreference = "editorFirst",
+            -- lineLength = 88,
+
+            -- Code Actions
+            -- fixAll = true,
+            -- organizeImports = true,
+            -- codeAction = {
+            --   disableRuleComment = { enable = true },
+            --   fixViolation = { enable = true },
+            -- },
+
+            -- Linter Settings
+            -- lint = {
+            --   enable = true,
+            --   preview = false,
+            --   -- select = { "E", "F", "I", "UP", "B" },
+            --   -- ignore = { "E501" },
+            -- },
+
+            -- Formatter Settings
+            -- format = {
+            --   preview = false,
+            -- },
+          },
+        },
+      },
+
+      pyright = {
+        settings = {
+          pyright = {
+            -- Let Ruff handle import sorting
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = {
+              ignore = { "*" }
+            },
+          },
+        },
+      },
+
+      lua_ls = {
+        settings = {
+        --   Lua = {
+        --     workspace = { checkThirdParty = false },
+        --     telemetry = { enable = false },
+        --   },
+        -- },
+      },
+    },
+
+    -- 4. Server Setup Hooks & Capability Tweaks
+    -- setup = {
+    --   ruff = function()
+    --     -- Disable hover provider in Ruff so Pyright's richer docstrings/signatures take precedence
+    --     LazyVim.lsp.on_attach(function(client, _)
+    --       if client.name == "ruff" then
+    --         client.server_capabilities.hoverProvider = false
+    --       end
+    --     end)
+    --   end,
+    },
+  },
+}
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,4 +1,22 @@
-return {
+
+local function get_motherboard_name()
+	local file = io.open("/sys/devices/virtual/dmi/id/board_name")
+
+	if not file then
+		print("motherboard_name doesn't exist")
+		return nil
+	end
+
+	local motherboard_name = file:read("*a")
+	file:close()
+	motherboard_name = motherboard_name:gsub("^%s*(.-)%s*$", "%1")
+
+	return motherboard_name
+end
+local motherboard_name = get_motherboard_name()
+
+
+if motherboard_name ~= "MS-16JF" then return {
   "folke/snacks.nvim",
   opts = {
     dashboard = {
@@ -17,3 +35,23 @@ return {
     },
   },
 }
+else return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = [[
+          ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+          ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
+          ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
+          ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
+          ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
+          ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
+
+[ @saka404 ]
+        ]],
+      },
+    },
+  },
+}
+end
