e10ed6593e31ace63d5adbab53f70543f3886e52
saka404 <67270824+Saka-CS@users.noreply.github.com>
2026-09-15 01:49:14 +0300

    Updated Lazyvim ASCII Art

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,27 +1,27 @@
-
 local function get_motherboard_name()
-	local file = io.open("/sys/devices/virtual/dmi/id/board_name")
+  local file = io.open("/sys/devices/virtual/dmi/id/board_name")
 
-	if not file then
-		print("motherboard_name doesn't exist")
-		return nil
-	end
+  if not file then
+    print("motherboard_name doesn't exist")
+    return nil
+  end
 
-	local motherboard_name = file:read("*a")
-	file:close()
-	motherboard_name = motherboard_name:gsub("^%s*(.-)%s*$", "%1")
+  local motherboard_name = file:read("*a")
+  file:close()
+  motherboard_name = motherboard_name:gsub("^%s*(.-)%s*$", "%1")
 
-	return motherboard_name
+  return motherboard_name
 end
 local motherboard_name = get_motherboard_name()
 
 
-if motherboard_name ~= "MS-16JF" then return {
-  "folke/snacks.nvim",
-  opts = {
-    dashboard = {
-      preset = {
-        header = [[
+if motherboard_name ~= "MS-16JF" then
+  return {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
           ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
           ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
           ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
@@ -31,16 +31,17 @@ if motherboard_name ~= "MS-16JF" then return {
 
 [ @saka404 at Twitch & YouTube ]
         ]],
+        },
       },
     },
-  },
-}
-else return {
-  "folke/snacks.nvim",
-  opts = {
-    dashboard = {
-      preset = {
-        header = [[
+  }
+else
+  return {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
           ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
           ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
           ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
@@ -50,8 +51,8 @@ else return {
 
 [ @saka404 ]
         ]],
+        },
       },
     },
-  },
-}
+  }
 end
